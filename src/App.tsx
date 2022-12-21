30ad0a15c1d3558d9405c14574906211c868cbd6
gitbush-test <82594147+megi-j@users.noreply.github.com>
2022-12-21 17:42:16 +0400

    added quotes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useRef } from "react";
+import "./App.css";
+import Moment from "react-moment";
+import photo from "./images/photo.png";
 
 function App() {
+  const [time, setTime] = useState<string>();
+  const [quote, setQuote] = useState<any>({});
+  // const [author, setAuthor] = useState<string>("");
+  const firstRander = useRef(true);
+  function getTime() {
+    fetch("https://worldtimeapi.org/api/ip/")
+      .then((response) => response.json())
+      .then((result) => {
+        setTime(result.datetime);
+      });
+  }
+  useEffect(() => {
+    const timeInterval = setInterval(() => {
+      getTime();
+    }, 1000);
+    return () => clearInterval(timeInterval);
+  }, []);
+
+  function getQuote() {
+    fetch("https://api.quotable.io/random")
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        setQuote(result);
+      });
+  }
+  useEffect(() => {
+    if (firstRander.current) {
+      getQuote();
+      firstRander.current = false;
+    }
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img src={photo} alt="" />
+      <div className="quote-box">
+        <p>{quote.content}</p>
+        <h5>{quote.author}</h5>
+      </div>
+      <Moment
+        style={{
+          position: "absolute",
+          bottom: 142,
+          left: 165,
+          fontSize: 200,
+          color: "#fff",
+          fontWeight: "bold",
+        }}
+        format="hh:mm"
+      >
+        {time}
+      </Moment>
     </div>
   );
 }
